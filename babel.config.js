924e2c5dfa8363589ec252993e106ff54c196d85
Richard Gutierrez Condori <rychard.gutierrez@gmail.com>
2023-02-09 01:25:46 -0400

    Add configuration jest

---
+++ babel.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+  // presets: ['@vue/cli-plugin-babel/preset'],
+};
---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  globals: {},
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.vue$': 'vue3-jest',
+    '^.+\\js$': 'babel-jest',
+  },
+  moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
